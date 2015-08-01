2878a88ab206463ce8d317b8dc1769a3fd266c98
Matthew Barulic <matthew.barulic@gmail.com>
2015-07-31 20:39:08 -0700

    Adds Ultrasonic sensor demo.

---
+++ 2-Basic-Robotics/UltrasonicDemo/UltrasonicDemo.ino
@@ -0,0 +1,35 @@
+#include <Wire.h>
+
+#include <Ultrasonic.h>
+#include <Motor.h>
+#include <Bricktronics.h>
+
+Bricktronics brick;
+Motor left_motor(&brick, 1);
+Motor right_motor(&brick, 2);
+Ultrasonic ultrasonic(&brick, 4);
+
+void setup() {
+  Serial.begin(9600);
+  brick.begin();
+  left_motor.begin();
+  right_motor.begin();
+  ultrasonic.begin();
+}
+
+void loop() {
+  unsigned char distance = ultrasonic.getDistance();
+  if(distance == 255) {
+    left_motor.stop();
+    right_motor.stop();
+  } else if(distance > 30) {
+    left_motor.set_speed(255);
+    right_motor.set_speed(255);
+  } else if(distance < 25) {
+    left_motor.set_speed(-255);
+    right_motor.set_speed(-255);
+  } else {
+    left_motor.stop();
+    right_motor.stop();
+  }
+}
